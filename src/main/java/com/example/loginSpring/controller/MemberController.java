30b4437b3951a15b1bbb2e1d7ad8ec4df92403e7
전제서 <wpti0416@gmail.com>
2025-08-28 19:44:19 +0900

    회원가입 수정

--- src/main/java/com/example/loginSpring/controller/MemberController.java
+++ src/main/java/com/example/loginSpring/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.example.loginSpring.controller;
 
+import com.example.loginSpring.dto.LoginRequestDTO;
 import com.example.loginSpring.dto.SignUpRequestDTO;
 import com.example.loginSpring.service.MemberService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,12 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
+    // 회원가입
     @PostMapping("/signUp")
     public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDTO signUpRequestDTO) {
         try {
@@ -29,7 +33,8 @@ public class MemberController {
         catch(Exception e) {
             return ResponseEntity.status(500).body("서버 오류 : " + e.getMessage()); // 500
         }
-
-
     }
+
+
+
 }
